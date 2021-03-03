609fa203f39b6ded2e485120f443a389925a1c00
Risky Jr <2114768901@qq.com>
2021-03-03 17:49:17 +0800

    滤波器

--- attenuation_model/script/compare_filter.m
+++ attenuation_model/script/compare_filter.m
@@ -35,3 +35,10 @@ legend({'mean','median','logarithmic'})
 
 %% 
 clc;
+% 生成一维高斯滤波模板
+r = 3;sigma = 1;
+GaussTemp = ones(1,r*2-1);
+
+for i=1 : r*2-1
+    GaussTemp(i) = exp(-(i-r)^2/(2*sigma^2))/(sigma*sqrt(2*pi));
+end
