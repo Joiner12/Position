d20514a29f333d817a0f9201fa91da59153fa273
Risky Jr <2114768901@qq.com>
2021-03-31 18:02:59 +0800

    区域划分

--- component/prev_data_redcution_integrate_same_ap.m
+++ component/prev_data_redcution_integrate_same_ap.m
@@ -1,57 +1,72 @@
 function new_ap = prev_data_redcution_integrate_same_ap(ap, param)
-%功能：整合相同的ap数据
-%定义：new_ap = prev_data_redcution_integrate_same_ap(ap)
-%参数： 
-%    ap：待整合的所有ap数据
-%    param：函数参数,具体如下
-%           param.same_ap_judge_type：相同ap的判断方式
-%输出：
-%    new_ap：整合后的ap数据
-%备注：若整合过程中发现同一个ap存在不同的经纬度，依据整合顺序，更新为最新的经纬度
+    %功能：整合相同的ap数据
+    %定义：new_ap = prev_data_redcution_integrate_same_ap(ap)
+    %参数：
+    %    ap：待整合的所有ap数据
+    %    param：函数参数,具体如下
+    %           param.same_ap_judge_type：相同ap的判断方式
+    %输出：
+    %    new_ap：整合后的ap数据
+    %备注：若整合过程中发现同一个ap存在不同的经纬度，依据整合顺序，更新为最新的经纬度
 
-    new_ap = [];
+    if false
+        %{
+        此类型的变量不支持使用点进行索引。
+
+        出错 prev_data_redcution_integrate_same_ap (line 51)
+        new_ap(new_ap_num).name = ap(i).name;
+
+        %}
+        new_ap = [];
+    else
+        new_ap = struct([]);
+    end
+
     ap_num = length(ap);
     new_ap_num = 0;
     same_ap_judge_type = param.same_ap_judge_type;
-    
+
     for i = 1:ap_num
         isfind = 0;
-        
+
         for j = 1:new_ap_num
-            if is_same_ap(same_ap_judge_type,...
-                          new_ap(j).name, ap(i).name,...
-                          new_ap(j).mac, ap(i).mac)
+
+            if is_same_ap(same_ap_judge_type, ...
+                    new_ap(j).name, ap(i).name, ...
+                    new_ap(j).mac, ap(i).mac)
                 %已经找到过相同的ap
                 isfind = 1;
                 recv_rssi_len = length(new_ap(j).recv_rssi);
                 new_ap(j).recv_rssi(recv_rssi_len + 1) = ap(i).rssi;
-                
+
                 if (new_ap(j).lat ~= ap(i).lat) ...
-                    || (new_ap(j).lon ~= ap(j).lon)
+                        || (new_ap(j).lon ~= ap(j).lon)
                     warning(['%s(%s)出现不同的经纬度,原经度：%s, 原纬度：%s,', ...
-                             '经度更新为：%s, 纬度更新为：%s'], ...
-                             new_ap(j).name, new_ap(j).mac, new_ap(j).lon, ...
-                             new_ap(j).lat, ap(i).lon, ap(i).lat);
-                         
+                            '经度更新为：%s, 纬度更新为：%s'], ...
+                        new_ap(j).name, new_ap(j).mac, new_ap(j).lon, ...
+                        new_ap(j).lat, ap(i).lon, ap(i).lat);
+
                     new_ap(j).lat = ap(i).lat;
                     new_ap(j).lon = ap(i).lon;
                 end
-                
+
                 break;
             end
+
         end
-        
+
         if isfind == 0
             %新的ap，增加到输出中
             new_ap_num = new_ap_num + 1;
-            new_ap(new_ap_num).name = ap(i).name; 
-            new_ap(new_ap_num).mac = ap(i).mac; 
-            new_ap(new_ap_num).lat = ap(i).lat; 
-            new_ap(new_ap_num).lon = ap(i).lon; 
-            new_ap(new_ap_num).recv_rssi(1) = ap(i).rssi; 
+            new_ap(new_ap_num).name = ap(i).name;
+            new_ap(new_ap_num).mac = ap(i).mac;
+            new_ap(new_ap_num).lat = ap(i).lat;
+            new_ap(new_ap_num).lon = ap(i).lon;
+            new_ap(new_ap_num).recv_rssi(1) = ap(i).rssi;
             new_ap(new_ap_num).rssi_reference = ap(i).rssi_reference;
             new_ap(new_ap_num).rssi = [];
-            
         end
+
     end
+
 end
--- data_analysis/area_divider.m
+++ data_analysis/area_divider.m
@@ -36,8 +36,9 @@ buffer = zeros(1, buffer_size);
 ap_rssi = {buffer, buffer, buffer, buffer, buffer, ...
             buffer, buffer, buffer};
 stored_data = struct('name', ap_name, 'mac', '', ...
-    'rssi', ap_rssi, 'lat', 0, 'lon', 0); % 缓存ap(t-n)数据
-tcf('shows'); figure('name', 'shows', 'color', 'w');
+    'rssi', ap_rssi, 'lat', 0, 'lon', 0, 'all_rssi', -90); % 缓存ap(t-n)数据
+
+figure_flag = true;
 
 for j = 1:1:length(file_ap_msg)
     file_ap_temp = file_ap_msg{j};
@@ -45,32 +46,93 @@ for j = 1:1:length(file_ap_msg)
     for k = 1:1:length(file_ap_temp)
         fprintf('----------------------%0.0f帧----------------------\n', k);
         frame_temp = file_ap_temp{k};
+        ap_got = zeros([1, 8]);
 
         for ki = 1:1:length(frame_temp)
             cur_apinfo = frame_temp(ki);
             index = find(strcmp(cur_apinfo.name, ap_name));
-            cur_rssi = cur_apinfo.rssi;
-            cur_rssi = reshape(cur_rssi, [length(cur_rssi), 1]);
-            stored_data(index).rssi = [cur_rssi, ...
-                                        stored_data(index).rssi(1:end - length(cur_rssi))];
-            stored_data(index).lat = cur_apinfo.lat;
-            stored_data(index).lon = cur_apinfo.lon;
+            % 只记录一次
+            if isequal(ap_got(index), 0)
+                ap_got(index) = 1;
+                cur_rssi = cur_apinfo.rssi;
+                cur_rssi = reshape(cur_rssi, [length(cur_rssi), 1]);
+
+                stored_data(index).rssi = [cur_rssi, ...
+                                            stored_data(index).rssi(1:end - length(cur_rssi))];
+                % 所有历史数据
+                stored_data(index).all_rssi = [stored_data(index).all_rssi, mean(cur_rssi)];
+                stored_data(index).lat = cur_apinfo.lat;
+                stored_data(index).lon = cur_apinfo.lon;
+            end
+
         end
 
-        % figure
-        cla;
-        hold on
-        for kj = 1:1:length(stored_data)
-            markers = ['*','<','>','s','d','+','x','p'];
-            plot(stored_data(kj).rssi,'Linewidth',1.5,'marker',markers(kj));
+        % 填充未收集到的rssi
+        ap_got = ~ap_got;
+        ap_nul = find(ap_got);
+
+        for kk = 1:1:length(ap_nul)
+            stored_data(ap_nul(kk)).all_rssi = [stored_data(ap_nul(kk)).all_rssi, -90];
         end
 
-        hold off
-        ap_leg = strrep(ap_name, '_', '-');
-        legend(ap_leg)
-        pause(1);
     end
 
 end
 
 % end
+
+%% 整体趋势分析
+if figure_flag
+    tcf('shows'); figure('name', 'shows', 'color', 'w');
+    markers = ['*', '<', '>', 's', 'd', '+', 'x', 'p'];
+    hold on
+
+    for kj = 1:1:length(stored_data)
+        plot(stored_data(kj).all_rssi, 'Linewidth', 1.5, 'marker', markers(kj));
+    end
+
+    hold off
+
+    ap_leg = strrep(ap_name, '_', '-');
+    legend(ap_leg)
+end
+
+%% 分段分析
+if figure_flag
+    scope = 10;
+    ap_all_rssi = cell(0);
+
+    for i = 1:1:length(stored_data)
+        ap_all_rssi{i} = stored_data(i).all_rssi;
+    end
+
+    valid_rssi = cell(0);
+    tcf('shows'); figure('name', 'shows', 'color', 'w');
+    markers = ['*', '<', '>', 's', 'd', '+', 'x', 'p'];
+    hold on
+
+    for k = 1:1:int16(length(ap_all_rssi{1}) / scope)
+
+        for i = 1:1:length(stored_data)
+            ap_rssi_temp = ap_all_rssi{i};
+            ap_rssi_temp = ap_rssi_temp(10 * (k - 1) + 1:10 * k);
+            ap_rssi_temp = ap_rssi_temp(ap_rssi_temp ~= -90);
+            valid_rssi{k, i} = ap_rssi_temp;
+        end
+
+    end
+
+    % draw figure
+    x = zeros(0);        
+    y = zeros(0);
+    for j = 1:1:size(valid_rssi, 1)
+        x = [x,linspace(1, 8, 8) + (j - 1) * 5];
+
+        for k = 1:1:size(valid_rssi, 2)
+            y((j - 1) * 8 + k) = length(valid_rssi{j, k});
+        end
+    end
+    
+    bar(y)
+    box on
+end
