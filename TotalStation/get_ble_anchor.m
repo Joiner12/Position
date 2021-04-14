5d376a63cf86a31dd64264ea17facafba0e6388c
Risky Jr <2114768901@qq.com>
2021-04-14 17:21:54 +0800

    修改全站仪数据

--- TotalStation/get_ble_anchor.m
+++ TotalStation/get_ble_anchor.m
@@ -15,23 +15,25 @@ ap_index = linspace(1, 8, 8);
 name = ['onepos_HLK_1', 'onepos_HLK_2', 'onepos_HLK_3', ...
         'onepos_HLK_4', 'onepos_HLK_5', 'onepos_HLK_6', ...
         'onepos_HLK_7', 'onepos_HLK_8'];
-% lat = bt_tab.lat;
-% lon = bt_tab.lon;
-% anchor_tb.name = name(ap_index);
-% anchor_tb.lat = lat(ap_index);
-% anchor_tb.lon = lon(ap_index);
 
-position = {cursor_info_1.Position, cursor_info_2.Position, ...
-            cursor_info_3.Position, cursor_info_4.Position, ...
-            cursor_info_5.Position, cursor_info_6.Position, ...
-            cursor_info_7.Position, cursor_info_8.Position};
+% À¶ÑÀÐÅ±ê
+position_ble = {cursor_info_1.Position, cursor_info_2.Position, ...
+                cursor_info_3.Position, cursor_info_4.Position, ...
+                cursor_info_5.Position, cursor_info_6.Position, ...
+                cursor_info_7.Position, cursor_info_8.Position};
+% Öù×Ó
+position_pillar = {cursor_info_pillar_1.Position, cursor_info_pillar_2.Position, ...
+                    cursor_info_pillar_3.Position, cursor_info_pillar_4.Position};
 
-for k = 1:1:length(position)
-    temp = position{k};
-    fprintf('******BLE:%.0f******\n',k);
-    fprintf('lat:%0.12f\nlon:%0.12f\n',temp(1),temp(2));
-    
-    % disp(position{k});
+for k = 1:1:length(position_ble)
+    temp = position_ble{k};
+    fprintf('******BLE:%.0f******\n', k);
+    fprintf('lat:%0.12f\nlon:%0.12f\n', temp(1), temp(2));
 end
 
+for k = 1:1:length(position_pillar)
+    temp = position_pillar{k};
+    fprintf('******PILLAR:%.0f******\n', k);
+    fprintf('lat:%0.12f\nlon:%0.12f\n', temp(1), temp(2));
+end
 % end
