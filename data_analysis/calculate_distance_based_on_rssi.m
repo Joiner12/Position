38a9943325000ae51581fbd55f8c075ee6ccca1b
Risky Jr <2114768901@qq.com>
2021-04-23 16:20:43 +0800

    修改对数模型计算接口

--- data_analysis/calculate_distance_based_on_rssi.m
+++ data_analysis/calculate_distance_based_on_rssi.m
@@ -1,21 +1,18 @@
-function distance = calculate_distance_based_on_rssi(model_parameter,rssi,varargin)
+function distance = calculate_distance_based_on_rssi(A,b,rssi,varargin)
 % 功能:
-%   根据拟合的对数路径损耗模型(Logarithmic path loss model )，计算RSSI对应的路径。
-% 定义: distance = calculate_distance_based_on_rssi(model_parameter,rssi,varargin)
+%       根据拟合的对数路径损耗模型(Logarithmic path loss model )，计算RSSI对应的路径。
+% 定义: 
+%       function distance = calculate_distance_based_on_rssi(A,b,rssi,varargin)
 % 参数:
-%   model_parameter(struct):损耗模型
-%   {
-%       double A(距离1m时，设备接收到无线信号的RSSI值);
-%       doubel n(衰减因子)
-%   }
-%   rssi:RSSI(array)
-%   varargin:保留参数
+%       A,距离1m时，设备接收到无线信号的RSSI值;
+%       b,衰减因子;
+%       rssi,RSSI(array)
+%       varargin:保留参数
 % 输出：
-%   distance:距离(array)
+%       distance:距离(array)
 
 %% 模型配置
-A = model_parameter.A;
-n_10 = 10*model_parameter.n;
+n_10 = 10*b;
 
 %% 计算
 dist_temp = zeros(0);
