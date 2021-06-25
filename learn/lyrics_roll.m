02fdc809295063571b9668d2869b3847b4a970e3
Risky Jr <2114768901@qq.com>
2021-06-25 17:56:13 +0800

    lyrics

---
+++ learn/lyrics_roll.m
@@ -0,0 +1,29 @@
+function lyrics_roll(varargin)
+    % 导入歌词文件
+    lyricfile = '';
+
+    if any(strcmpi(varargin, 'lyrics_file'))
+        lyricfile = varargin{find(strcmpi(varargin, 'lyrics_file')) + 1};
+    else
+        [file, path] = uigetfile('*.lrc*', 'multiselect', 'off');
+        lyricfile = fullfile(path, file);
+    end
+
+    lyric_id = open(lyricfile, 'r');
+    lyric = cell(0);
+
+    while (~eof(lyric_id))
+        lyric{length(lyric) + 1, :} = fgetl(lyric_id);
+    end
+
+    fclose(lyric_id);
+end
+
+function parsed_lyric = parse_lyric(line_piece)
+    parsed_lyric = cell(0);
+
+    if ~ischar(line_piece)
+        error('line is not char');
+    end
+
+end
