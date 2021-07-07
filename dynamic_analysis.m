4b69ad27b0ec0a79e4be57c0a7616ca90fd68d7a
Risky Jr <2114768901@qq.com>
2021-07-07 17:47:53 +0800

    静态定位过程调试

--- dynamic_analysis.m
+++ dynamic_analysis.m
@@ -3,3 +3,14 @@ clc;
 % markdown_tool.batch_remove('D:\Code\BlueTooth\pos_bluetooth_matlab\Doc\img\temp-location-1');
 markdown_tool.write_to_markdown('D:\Code\BlueTooth\pos_bluetooth_matlab\Doc\定位过程分析.md', ...
     'D:\Code\BlueTooth\pos_bluetooth_matlab\Doc\img\temp-location-1');
+
+%% 
+clc;
+load('D:\Code\BlueTooth\pos_bluetooth_matlab\temp.mat');
+distance = calc_distance_based_on_rssi(trilateration_ap(2));
+disp(distance)
+
+%%
+clc;
+disp(pwd)
+call_me();
