07af7cbe6417729cc309c6646ba462b248a4288d
Risky Jr <2114768901@qq.com>
2021-07-21 17:32:52 +0800

    瑞利信道分析-1

--- attenuation_model/script/analysis_channle.m
+++ attenuation_model/script/analysis_channle.m
@@ -11,6 +11,37 @@ end
 
 files_data = reshape(files_data, [length(files_data), 1]);
 % get_std_detail_from_file(strcat(file_head,num2str(j),'m.txt'));
+
+%% transport and receive
+clc;
+%-----------Parameters Setting------------
+LightSpeedC = 3e8;
+BlueTooth = 2400 * 1000000; %hz
+Zigbee = 915.0e6; %hz
+Freq = BlueTooth;
+TXAntennaGain = 0.5; %db
+RXAntennaGain = 1; %db
+PTx = 0.001; %watt
+sigma = 6; %Sigma from 6 to 12 %Principles of communication systems simulation with wireless application P.548
+mean = 0;
+PathLossExponent = 2; %Line Of sight
+
+%------------ FRIIS Equation --------------
+% Friis free space propagation model:
+%        Pt * Gt * Gr * (Wavelength^2)
+%  Pr = --------------------------
+%        (4 *pi * d)^2 * L
+
+pr_set = [];
+
+for Dref = 0:1:1000
+    Wavelength = LightSpeedC / Freq;
+    PTxdBm = 10 * log10(PTx * 1000);
+    M = Wavelength / (4 * pi * Dref);
+    Pr0 = PTxdBm + TXAntennaGain + RXAntennaGain - (20 * log10(1 / M));
+    pr_set = [pr_set, Pr0];
+end
+
 %% get std data from file,transfer into structs.
 function std_data = get_std_detail_from_file(full_file_name)
     std_data = struct();
