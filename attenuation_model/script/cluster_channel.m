1860c39b392622949aab21d15a4675110863a6a3
Risky Jr <2114768901@qq.com>
2021-09-08 18:36:24 +0800

    聚类结果分析

--- attenuation_model/script/cluster_channel.m
+++ attenuation_model/script/cluster_channel.m
@@ -9,18 +9,18 @@ rssi_39 = get_rssi_info(['D:\Code\BlueTooth\pos_bluetooth_matlab\attenuation_mod
                         'channel-1\ope_9-100ms-2-ch39.txt'], 'ope_9');
 rssi_fusion = get_rssi_info(['D:\Code\BlueTooth\pos_bluetooth_matlab\attenuation_model\data\', ...
                             'channel-1\ope_9-100ms-2-ch-fusion.txt'], 'ope_9');
+data_rssi_y_3 = rssi_fusion(5:34);
+data_rssi_y_5 = data_rssi_y_3;
+data_rssi_x_3 = 1:1:length(data_rssi_y_3);
+data_rssi_x_5 = 1:1:length(data_rssi_y_5);
 %% k-means THREE clustering
 clc;
-data_rssi_y_3 = rssi_fusion(1:300);
-data_rssi_x_3 = 1:1:length(data_rssi_y_3);
 opts = statset('Display', 'final');
-[idx_3, C_3] = kmeans(data_rssi_y_3, 3, 'Distance', 'sqeuclidean', ...
+[idx_3, C_3] = kmeans(data_rssi_y_3, 3, 'Distance', 'cityblock', ...
     'Replicates', 5, 'Options', opts);
 %% k-means FIVE clustering
-data_rssi_y_5 = rssi_fusion(1:300);
-data_rssi_x_5 = 1:1:length(data_rssi_y_5);
 opts = statset('Display', 'final');
-[idx_5, C_5] = kmeans(data_rssi_y_3, 5, 'Distance', 'sqeuclidean', ...
+[idx_5, C_5] = kmeans(data_rssi_y_3, 5, 'Distance', 'cityblock', ...
     'Replicates', 5, 'Options', opts);
 %% figure-1 different channel compare
 tcf('ch-figure-1'); figure('name', 'ch-figure-1', 'color', 'w');
@@ -41,7 +41,7 @@ title('38');
 subplot(224)
 plot(rssi_39);
 title('39');
-
+tcf('ch-figure-1');
 %% figure-2 k-means THREE clustering
 tcf('clustering-1');
 figure('color', 'white', 'name', 'clustering-1');
@@ -57,7 +57,7 @@ line([1, length(data_rssi_y_3)], [C_3(3, 1), C_3(3, 1)], 'LineWidth', 1, 'color'
 legend('rssi', 'Cluster 1', 'Cluster 2', 'Cluster 3', 'Location', 'NW')
 title('Cluster Assignments and Origin')
 hold off
-
+tcf('clustering-1');
 %% figure-3 k-means FIVE clustering
 tcf('clustering-2');
 figure('color', 'white', 'name', 'clustering-2');
@@ -77,3 +77,18 @@ legend('rssi', 'Cluster 1', 'Cluster 2', 'Cluster 3', ...
     'Cluster 4', 'Cluster 5', 'Location', 'NW')
 title('Cluster Assignments and Origin')
 hold off
+tcf('clustering-2');
+%% mean val compare
+disp('mean values compare');
+mean_C3 = sort(C_3);
+mean_C5 = sort(C_5);
+mean_origin = sort([mean(rssi_37), mean(rssi_38), mean(rssi_39)]);
+tcf('mean-1');
+figure('color', 'white', 'name', 'mean-1');
+hold on
+plot([2, 3, 4], mean_C3, 'Marker', '*', 'LineWidth', 1.5);
+plot([1, 2, 3, 4, 5], mean_C5, 'Marker', 'o', 'LineWidth', 1.5);
+plot([2, 3, 4], mean_origin, 'Marker', '+', 'LineWidth', 1.5);
+text([2, 3, 4], mean_origin, {'Ch39', 'Ch37', 'Ch38'});
+legend('clustering-3', 'clustering-5', 'single Ch')
+grid on
