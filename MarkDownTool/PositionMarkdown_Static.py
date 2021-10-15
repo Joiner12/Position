75903921e13614efd644925731b933d8758f6b36
Risky Jr <2114768901@qq.com>
2021-10-15 14:01:28 +0800

    改用python实现markdown文件

---
+++ MarkDownTool/PositionMarkdown_Static.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Oct 15 11:10:45 2021
+
+@author: W-H
+"""
+
+import dominate
+from dominate.tags import *
+from datetime import datetime
+
+picName = ['P0', 'P1', 'P2', 'P3', 'P4', 'P5', 'P6', 'P11', 'P12', 'P13', 'P14',
+           'P15', 'P16', 'P17', 'P18', 'P19', 'P20', 'P21', 'P22', 'P23', 'P24', 'P25']
+doc = dominate.document(title=r"static positioning ")
+doc.body['style'] = "text-align:center;"
+with doc:
+    h1("静态位置定位结果", style="font-size:45px;color: #231820;font-weight: Bold;")
+    p('Update time:'+datetime.now().strftime('%Y-%m-%d %H:%M:%S'),
+      style="font-size:20px;text-align:center;color:#6D7F7D")
+# <div>
+#         <img src="D:\Code\BlueTooth\pos_bluetooth_matlab\Doc\img\static-P0-2.png" style="zoom:110%;">
+#         <p style="font-size:30px;">P0</p>
+#     </div>
+with doc.body:
+    for k in picName:
+        curDiv = div()
+        imgFileName = r"D:\Code\BlueTooth\pos_bluetooth_matlab\Doc\img\static-" + \
+            str(k)+r"-2.png"
+        curDiv.add(p(k+"定位结果", style="font-size:30px;color:#6D7F7D"))
+        curDiv.add(img(src=imgFileName, style="zoom:120%"))
+
+with open(r'D:\Code\BlueTooth\pos_bluetooth_matlab\Doc\静态点定位结果-1.html', 'w', encoding='utf-8') as f:
+    f.write(doc.render())
+print('Update static positioning', datetime.now().strftime('%Y-%m-%d %H:%M:%S'))
